e1910f4473285bef733684c4d76f8ec2ff2a09df
Jakub Winiarski <jakub.winiarski2.stud@pw.edu.pl>
2023-06-29 22:15:14 +0200

    Validate input on edit user

--- src/ViewModelLayer/ViewModels/EditUserViewModel.cs
+++ src/ViewModelLayer/ViewModels/EditUserViewModel.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using ViewModelLayer.Commands;
@@ -24,6 +25,50 @@ namespace ViewModelLayer.ViewModels
             set { _usersStore.CurrentUser = value; }
         }
 
+        public string CurrentUserName
+        {
+            get { return _usersStore.CurrentUser.name; }
+            set
+            {
+                CheckForErrors(value, @"^[A-Za-z ]+$", nameof(CurrentUserName));
+                OnPropertyChanged(nameof(HasNoErrors));
+                _usersStore.CurrentUser.name = value;
+            }
+        }
+
+        public string CurrentUserSurname
+        {
+            get { return _usersStore.CurrentUser.surename; }
+            set
+            {
+                CheckForErrors(value, @"^[A-Za-z ]+$", nameof(CurrentUserSurname));
+                OnPropertyChanged(nameof(HasNoErrors));
+                _usersStore.CurrentUser.surename = value;
+            }
+        }
+
+        public string CurrentUserEmail
+        {
+            get { return _usersStore.CurrentUser.email; }
+            set
+            {
+                CheckForErrors(value, @"^([\w\.\-]+)@([\w\-]+)((\.(\w){1,3})+)$", nameof(CurrentUserEmail));
+                OnPropertyChanged(nameof(HasNoErrors));
+                _usersStore.CurrentUser.email = value;
+            }
+        }
+
+        public string CurrentUserPhone
+        {
+            get { return _usersStore.CurrentUser.phone; }
+            set
+            {
+                CheckForErrors(value, @"[0-9]+$",nameof(CurrentUserPhone));
+                OnPropertyChanged(nameof(HasNoErrors));
+                _usersStore.CurrentUser.phone = value;
+            }
+        }
+
         public ICommand Cancel { get; }
         public ICommand Save { get; }
 
@@ -35,16 +80,30 @@ namespace ViewModelLayer.ViewModels
             _propertyNameToErrorsDictionary = new Dictionary<string, List<string>>();
         }
 
-
         // Error handling
         private readonly Dictionary<string, List<string>> _propertyNameToErrorsDictionary;
         public bool HasErrors => _propertyNameToErrorsDictionary.Any();
+        public bool HasNoErrors => !HasErrors;
 
         public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
 
         public IEnumerable GetErrors(string? propertyName)
         {
             return _propertyNameToErrorsDictionary.GetValueOrDefault(propertyName, new List<string>());
+        }
+
+        private void CheckForErrors(string value, String regex, string nameOfProperty)
+        {
+            _propertyNameToErrorsDictionary.Remove(nameOfProperty);
+            if (string.IsNullOrEmpty(value) || !Regex.IsMatch(value, regex))
+            {
+                List<string> errors = new List<string>()
+                    {
+                        $"The {nameOfProperty} is wrong or empty"
+                    };
+                _propertyNameToErrorsDictionary.Add(nameOfProperty, errors);
+                ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(nameOfProperty));
+            }
         }
     }
 }
